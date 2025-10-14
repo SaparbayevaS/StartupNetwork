3c62fab63a7526165538143c7931fc00863c2868
sabina <sa_saparbaeva@kbtu.kz>
2025-10-14 11:08:33 +0500

    Fixed some details

--- startup_network/asgi.py
+++ startup_network/asgi.py
@@ -4,11 +4,9 @@ ASGI config for startup_network project.
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-<<<<<<< HEAD
+
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-=======
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
->>>>>>> origin/bekarys
+
 """
 
 import os
--- startup_network/wsgi.py
+++ startup_network/wsgi.py
@@ -4,11 +4,9 @@ WSGI config for startup_network project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-<<<<<<< HEAD
+
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-=======
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
->>>>>>> origin/bekarys
+
 """
 
 import os
