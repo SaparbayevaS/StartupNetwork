ca7765850552bf1f7bae688668a0e61382cba62c
sabina <sa_saparbaeva@kbtu.kz>
2025-12-02 23:26:12 +0500

    Endpoints and tests

--- core/admin.py
+++ core/admin.py
@@ -1,46 +1,40 @@
-from django.contrib import admin
+from django.contrib.admin import ModelAdmin, register
 from .models import CustomUser, UserProfile, Category, Idea, Comment, Vote
 
-# --- CustomUser ---
-@admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
+@register(CustomUser)
+class CustomUserAdmin(ModelAdmin):
     list_display = ("email", "full_name", "is_staff", "is_active", "created_at", "updated_at", "deleted_at")
     list_filter = ("is_staff", "is_active")
     search_fields = ("email", "full_name")
     readonly_fields = ("created_at", "updated_at", "deleted_at")
     ordering = ("-created_at",)
 
-# --- UserProfile ---
-@admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
+@register(UserProfile)
+class UserProfileAdmin(ModelAdmin):
     list_display = ("user", "total_likes", "avatar")
     search_fields = ("user__email", "user__full_name")
 
-# --- Category ---
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
+@register(Category)
+class CategoryAdmin(ModelAdmin):
     list_display = ("name", "created_at", "updated_at", "deleted_at")
     search_fields = ("name",)
     readonly_fields = ("created_at", "updated_at", "deleted_at")
 
-# --- Idea ---
-@admin.register(Idea)
-class IdeaAdmin(admin.ModelAdmin):
+@register(Idea)
+class IdeaAdmin(ModelAdmin):
     list_display = ("title", "author", "category", "created_at", "updated_at", "deleted_at")
     list_filter = ("category", "author")
     search_fields = ("title", "description", "author__email")
     readonly_fields = ("created_at", "updated_at", "deleted_at")
 
-# --- Comment ---
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
+@register(Comment)
+class CommentAdmin(ModelAdmin):
     list_display = ("idea", "author", "content", "created_at", "updated_at", "deleted_at")
     search_fields = ("content", "author__email", "idea__title")
     readonly_fields = ("created_at", "updated_at", "deleted_at")
 
-# --- Vote ---
-@admin.register(Vote)
-class VoteAdmin(admin.ModelAdmin):
+@register(Vote)
+class VoteAdmin(ModelAdmin):
     list_display = ("idea", "user", "created_at", "updated_at", "deleted_at")
     search_fields = ("user__email", "idea__title")
     readonly_fields = ("created_at", "updated_at", "deleted_at")
